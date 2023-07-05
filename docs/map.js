dc68baeaf2151dde9e1a0cedea3e139656bf7b94
Jean Millerat <jean.terracommuni@wecena.com>
2023-07-05 04:03:13 +0200

    try to add google satellite layer

--- docs/map.js
+++ docs/map.js
@@ -1,8 +1,19 @@
-const map = L.map('map');
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+  {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-    {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
-    .addTo(map);
+var googleLayer = L.gridLayer
+  .googleMutant({
+    type: "hybrid", // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+  });
+
+const map = L.map('map', layers[osmLayer, googleLayer]);
+
+osmLayer.addTo(map);
+googleLayer.addTo(map);
+
+var baseMaps = { "OSM": osmLayer, "Google": googleLayer };
+
+var layerControl = L.control.layers(baseMaps).addTo(map);
 
 let bounds = [];
 for (let i = 0; i < markers.length; i++ ) {
