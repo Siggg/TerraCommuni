6994f1671437991f4f9d57eef6e87fbfc7d14542
Jean Millerat <jean.terracommuni@wecena.com>
2023-07-05 04:33:20 +0200

    fix maps

--- static/map.js
+++ static/map.js
@@ -1,24 +1,25 @@
 var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
   {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
 
-var googleLayer = L.gridLayer
-  .googleMutant({
-    type: "hybrid", // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-  });
 
-const map = L.map('map', layers[osmLayer, googleLayer]);
+var mutantLayer = L.gridLayer
+ .googleMutant({
+    type: "satellite", // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+  });
+  
+const map = L.map('map'); //, layers[osmLayer, googleLayer]);
 
 osmLayer.addTo(map);
-googleLayer.addTo(map);
+mutantLayer.addTo(map);
 
-var baseMaps = { "OSM": osmLayer, "Google": googleLayer };
+var baseMaps = { "OSM": osmLayer, "Mutant": mutantLayer };
 
 var layerControl = L.control.layers(baseMaps).addTo(map);
 
 let bounds = [];
 for (let i = 0; i < markers.length; i++ ) {
     const marker = L.marker([markers[i].latitude, markers[i].longitude]).addTo(map);
-    marker.bindPopup(markers[i].date);
+    marker.bindPopup(markers[i].datetime + " by " + markers[i].organizer_identifier);
     bounds.push([markers[i].latitude, markers[i].longitude]);
 }
 
